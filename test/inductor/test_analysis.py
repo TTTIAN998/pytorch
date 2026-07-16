436c48e748284bf36e36cf1399820d935622c871
Yeqing Peng <pengyeqing@huawei.com>
2026-07-16 17:19:15 +0800

    [Test] Decouple test_analysis.py from CUDA-specific
    Summary:
    - Uses `torch.accelerator.is_available()` to detect the available accelerator type,
      because `has_supported_gpu` check GPU platform with Triton support is available.
    - Now XPU has supported `deviceProperties`, uses `torch.accelerator.is_available()`
      instead of hard-coding CUDA/HPU/XPU checks.
    
    This allows out-of-tree accelerators to be recognized correctly when
    deciding whether a inductor test has enough devices to run.
    
    Signed-off-by: Yeqing Peng <846295776@qq.com>

--- test/inductor/test_analysis.py
+++ test/inductor/test_analysis.py
@@ -29,7 +29,7 @@ from torch.testing._internal.common_utils import (
     TestCase,
     xfailIfNoAcceleratorTriton,
 )
-from torch.testing._internal.inductor_utils import IS_BIG_GPU
+from torch.testing._internal.inductor_utils import IS_BIG_GPU, HAS_TRITON
 
 
 example_profile = """
@@ -285,7 +285,7 @@ class TestUtils(TestCase):
 
 def has_supported_gpu():
     """Check if any GPU platform with Triton support is available."""
-    return torch.xpu.is_available() or SM80OrLater or torch.version.hip
+    return (torch.accelerator.is_available() and HAS_TRITON) or SM80OrLater or torch.version.hip
 
 
 class TestAnalysis(TestCase):
@@ -644,9 +644,8 @@ class TestAnalysis(TestCase):
 
         # Verify device properties are present
         self.assertIn("deviceProperties", combined_profile)
-        # XPU currently does not have the deviceProperties like CUDA.
-        # See https://github.com/intel/torch-xpu-ops/issues/2247
-        if torch.cuda.is_available():
+        acc = torch.accelerator.current_accelerator()
+        if acc is not None and torch.get_device_module(acc.type).is_available():
             self.assertGreater(len(combined_profile["deviceProperties"]), 0)
 
         # Verify some trace events from each original profile are present
